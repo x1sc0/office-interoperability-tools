a42892e4409ad6bc198286e0de452f2515fa0efc
Xisco Fauli <xiscofauli@libreoffice.org>
2020-05-02 16:03:35 +0200

    msoconv: improve output

--- scripts/msoconv.py
+++ scripts/msoconv.py
@@ -12,14 +12,16 @@
 # for the specific language governing rights and limitations under the
 # License.
 
+import sys
 import os
 import parser
-from subprocess import Popen, PIPE, TimeoutExpired, DEVNULL, run
+from subprocess import Popen, PIPE, DEVNULL, run
 import signal
 import string
 import random
 import multiprocessing
 import shutil
+import time
 
 def kill_mso(wineprefix):
     p = Popen(['ps', '-A'], stdout=PIPE)
@@ -47,18 +49,26 @@ def launch_OfficeConverter(fileName, arguments, count, total_count):
     outputName = os.path.join(arguments.output, fileName + ".pdf")
     shutil.copyfile(inputName, os.path.join('/tmp', fileName))
     os.chdir('/tmp/')
+
+    startTime = time.time()
     # Using timeout with popen fills up the memory and eventually the kernel throws a OSError: [Errno 12] Cannot allocate memory
-    # pass the timeout parameter instead
-    timeout = 100
+    # pass the timeout parameter instead. 60 should be more than enough. Most files are converted within 10 seconds
+    timeout = 60
     p = Popen(['timeout', str(timeout), 'xvfb-run', '-a', 'wine', 'OfficeConvert', '--format=pdf', fileName, "--output=" + tmpName],
         stdout=DEVNULL, stderr=DEVNULL)
     p.communicate()
+    endTime = time.time()
+    diffTime = int(endTime - startTime)
 
     if os.path.exists(tmpName):
         shutil.move('/tmp/' + tmpName, outputName)
-        print(str(count) + "/" + str(total_count) + " - Converted " + inputName + " to " + outputName)
+        print(str(count) + "/" + str(total_count) + " - SUCCESS: Converting " + inputName + " to " + outputName + ' after ' + str(diffTime) + ' seconds')
     else:
-        print(str(count) + "/" + str(total_count) + " - TIMEOUT: Converting " + inputName + " to " + outputName)
+        if diffTime >= timeout - 1:
+            print(str(count) + "/" + str(total_count) + " - TIMEOUT: Converting " + inputName + " to " + outputName + ' after ' + str(diffTime) + ' seconds')
+        else:
+            # Some files are corrupted
+            print(str(count) + "/" + str(total_count) + " - ERROR: Converting " + inputName + " to " + outputName + ' after ' + str(diffTime) + ' seconds')
 
     os.remove(os.path.join('/tmp', fileName))
 
@@ -80,7 +90,7 @@ if __name__ == "__main__":
 
     if listFiles:
         cpuCount = multiprocessing.cpu_count()
-        chunkSplit = cpuCount * 16
+        chunkSplit = cpuCount * 8
         count = 0
         total_count = len(listFiles)
 
